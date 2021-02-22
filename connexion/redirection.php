636fe07e8faa2c3b333ac41f4f9335a4e6e8ad72
Ahouri Paul <ahouripaul@gmail.com>
2021-02-22 11:21:34 +0100

    Correction d'erreur mineur

--- connexion/redirection.php
+++ connexion/redirection.php
@@ -16,7 +16,7 @@
     <div class="sucess">
     <h1>Bienvenue <?php echo $_SESSION['username']; ?>!</h1>
     <p>C'est votre tableau de bord.</p>
-    <a href="logout.php">Déconnexion</a>
+    <a href="deconnexion.php">Déconnexion</a>
     </div>
   </body>
 </html>
